763c4b7efe4ac766144d162dac388dc98dcf3049
vasily88 <vasek88@gmail.com>
2022-06-13 09:00:25 +0300

    discount

--- src/components/highlights/discount.js
+++ src/components/highlights/discount.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
+import {Fade, Slide} from 'react-awesome-reveal';
+
 const Discount = () => {
     return(
-        <div>
-            Discount
+        <div className='wrapperDiscount'>
+
+            <Slide right className='discountRight'>
+                <p>
+                    my name is description amd this is my web site, no imfre cdommg dofjg.
+                    kjfu ldlll siiii, dfifkdl kdjjfk ieeiei oowols dlfdklkflle slfkslfklf.
+                    my name is description amd this is my web site, no imfre cdommg dofjg.
+                    kjfu ldlll siiii, dfifkdl kdjj
+                </p>
+            </Slide>
+
+            <Fade 
+                delay={1000} 
+                className='discountLeft'
+                onVisibilityChange={(inView) => {
+                    if (inView){
+                        console.log(inView);
+                    }
+                }}
+                >
+                <p>
+                    <span>30%</span>
+                    <span>OFF</span>
+                </p>
+            </Fade>
+            
         </div>
     );
 }
